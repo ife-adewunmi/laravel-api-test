04227900fab4293fb4b2d08ad4a1b4fc372b5fe4
S. Mehran Abghari <mehran.ab80@gmail.com>
2021-12-05 00:04:20 +0300

    Tests

---
+++ tests/Feature/HotelTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Hotel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HotelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_resolving_active_hotel()
+    {
+        $hotel = Hotel::factory()->create();
+
+        $this->getJson(route('api.hotels.find', ['id' => $hotel->id]))
+            ->assertOk()
+            ->assertJsonStructure([
+                'hotel' => [
+                    'id',
+                    'name',
+                    'rating',
+                    'reviews',
+                ],
+            ])
+            ->assertJsonCount(0, 'hotel.reviews')
+            ->assertSeeText($hotel->name);
+    }
+
+    public function test_resolving_active_hotel_with_reviews()
+    {
+        $hotel = Hotel::factory()
+            ->hasReviews(3)
+            ->create();
+
+        $this->getJson(route('api.hotels.find', ['id' => $hotel->id]))
+            ->assertOk()
+            ->assertJsonStructure([
+                'hotel' => [
+                    'id',
+                    'name',
+                    'rating',
+                    'reviews',
+                ],
+            ])
+            ->assertJsonCount(3, 'hotel.reviews')
+            ->assertSeeTextInOrder([
+                $hotel->name,
+                $hotel->reviews[0]->title,
+                $hotel->reviews[1]->title,
+                $hotel->reviews[2]->title,
+            ]);
+    }
+
+    public function test_resolving_inactive_hotel()
+    {
+        $hotel = Hotel::factory()->inactive()->create();
+
+        $this->getJson(route('api.hotels.find', ['id' => $hotel->id]))
+            ->assertNotFound();
+    }
+}
